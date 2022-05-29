849511c9dff72b8a34bfa37694d127cb5c6cdec8
sanjeevanee dhavale <102053897+sanjivanidhavale@users.noreply.github.com>
2022-05-29 17:49:49 +0530

    Add files via upload

---
+++ May/Week5/Q4/script.js
@@ -0,0 +1,24 @@
+// var
+console.log("This is example of Var");
+var a =101;
+var b=102;
+console.log(a+b);
+document.write("Var Example Also see in console <br>");
+document.write(a+b+'<br>');
+// let
+
+let c=1;
+let d=2;
+console.log("This is example of let");
+console.log(c+d);
+document.write("let Example Also see in console <br>");
+document.write(c+d+'<br>');
+
+// const
+const e=2;
+const f=3;
+console.log("This is example of const");
+console.log(e+f);
+document.write("const Example Also see in console <br>");
+document.write(e+f+'<br>');
+
