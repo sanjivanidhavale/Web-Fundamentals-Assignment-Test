d7496428ab5fb2bab5875f1d9465e279b1603961
sanjeevanee dhavale <102053897+sanjivanidhavale@users.noreply.github.com>
2022-05-29 17:54:28 +0530

    Add files via upload

---
+++ May/Week5/Q5/script.js
@@ -0,0 +1,27 @@
+
+let weekname = "sunday";
+switch (weekname) {
+    case "monday":
+        console.log("Monday working day.");
+        break;
+    case "tuesday":
+        console.log("Tuesday working day.");
+        break;
+    case "wednesday":
+        console.log("Wednesday working day.");
+        break;
+    case "thursday":
+        console.log("Thursday working day.");
+        break;
+    case "friday":
+        console.log("Friday is a like funny.");
+        break;
+    case "saturday":
+        console.log("Saturday week end.");
+        break;
+    case "sunday":
+        console.log("Sunday government holiday");
+        break;
+    default:
+        console.log("No any case found.");
+}
